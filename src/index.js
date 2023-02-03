6b184fb18705b9ad82b7ce93b2da2ac18eefe739
nam.thanh.nguyen <nguyennam130798@gmail.com>
2023-02-04 01:23:11 +0900

    update

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Template engine
+app.engine('hbs', handlebars.engine({
+  extname:'.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname,'resources/views'));
+
+app.get('/', (req, res) => res.render('home'));
+app.get('/news', (req, res) => res.render('news'));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
